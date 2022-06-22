0ae5e850b441fb61b77413dcc10ae2d1461469c4
stla <laurent_step@yahoo.fr>
2022-06-22 15:53:57 +0200

    hsluv example

--- R/hsluv.R
+++ R/hsluv.R
@@ -18,7 +18,30 @@ NULL
 #' @export
 #'
 #' @examples
-#' xx
+#' saturation <- 100
+#' f <- Vectorize(
+#'   function(x, y){
+#'     z <- complex(real = x, imaginary = y)
+#'     modulus <- Mod(z)
+#'     if(modulus > 1){
+#'       return("#ffffff")
+#'     }
+#'     radians <- Arg(z)
+#'     if(radians < 0){
+#'       radians <- radians + 2*pi
+#'     }
+#'     degrees <- 360 * radians / 2 / pi
+#'     hsluv(h = degrees, s = saturation, l = 100*modulus)
+#'   }
+#' )
+#' 
+#' x <- y <- seq(-1, 1, length.out = 200L)
+#' image <- outer(x, y, f)
+#' 
+#' opar <- par(mar = c(0, 0, 0, 0))
+#' plot(NULL, xlim = c(-1, 1), ylim = c(-1, 1), asp = 1)
+#' rasterImage(image, -1, -1, 1, 1)
+#' par(opar)
 hsluv <- function(h = 360, s = 100, l = 100){
   hsluv_cpp(h, s, l)
 }
--- inst/essais/circle.R
+++ inst/essais/circle.R
@@ -1,20 +1,25 @@
-x <- y <- seq(-1, 1, length.out = 100L)
+saturation <- 100
 f <- Vectorize(
   function(x, y){
     z <- complex(real = x, imaginary = y)
+    modulus <- Mod(z)
+    if(modulus > 1){
+      return("#ffffff")
+    }
     radians <- Arg(z)
     if(radians < 0){
       radians <- radians + 2*pi
     }
     degrees <- 360 * radians / 2 / pi
-    modulus <- Mod(z)
-    hsluv(h = degrees, s = modulus, l = 50)
+    hsluv(h = degrees, s = saturation, l = 100*modulus)
   }
 )
+
+x <- y <- seq(-1, 1, length.out = 200L)
 image <- outer(x, y, f)
 
 opar <- par(mar = c(0, 0, 0, 0))
-plot(NULL, xlim = c(-1, 1), ylim = c(-1, 1))
+plot(NULL, xlim = c(-1, 1), ylim = c(-1, 1), asp = 1)
 rasterImage(image, -1, -1, 1, 1)
 par(opar)
 
